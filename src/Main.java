9c6654087d3cb650cc4ef8a56393cdc1bcd8993d
Andrey-Afoniy <mr.knop2015@yandex.ru>
2025-06-25 17:53:55 +0400

    HW9 fix

--- src/Main.java
+++ src/Main.java
@@ -38,10 +38,13 @@ public class Main {
 
         System.out.println("Задание 4");
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
+        char temp;
+
+        for (int i = 0; i < reverseFullName.length / 2; i++) {
+            temp = reverseFullName[i];
+            reverseFullName [i] = reverseFullName[reverseFullName.length - 1 - i];
+            reverseFullName[reverseFullName.length - 1 -i] = temp;
         }
+        System.out.println(reverseFullName);
     }
     }
-
-
